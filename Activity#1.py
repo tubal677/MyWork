11dda367e8571ec6d5de0727a727e6c610436d8e
tubal677 <tubal.jacmar@gmail.com>
2026-02-05 14:54:15 +0800

    Add files via upload

---
+++ Activity#1.py
@@ -0,0 +1,12 @@
+score1 = 100
+score2 = 74
+
+if score1 >= 75:
+    print("Passed")
+else:
+    print("Failed")
+
+if score2 >= 75:
+    print("Passed")
+else:
+    print("Failed")
---
+++ Activity#2.py
@@ -0,0 +1,16 @@
+name = "Jacmar Tubal"
+age = 18
+height = 1.7018
+working = False
+
+print("\nFullname:", name)
+print(type(name))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working:", working)
+print(type(working))
